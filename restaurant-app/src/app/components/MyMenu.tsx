bbaaea8a1641b433b2f58748a5026de048b80d92
Serdar Shefketov <serdar.shefketov@lights.digital>
2024-05-15 14:55:31 +0300

    (lolivo): refactoring

--- restaurant-app/src/app/components/MyMenu.tsx
+++ restaurant-app/src/app/components/MyMenu.tsx
@@ -18,7 +18,72 @@ import HTMLFlipBook from "react-pageflip";
 // import Page_15 from "15.jpg";
 
 import Image from "next/image";
-
+const menuPages = [
+  {
+    page: 1,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=1.jpg&t=JQPxvHjzMkqD6xJ&scalingup=0",
+  },
+  {
+    page: 2,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=2.jpg&t=HtaKxxcMb9Gs37p&scalingup=0",
+  },
+  {
+    page: 3,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=3.jpg&t=764f0LsLEaD2URL&scalingup=0",
+  },
+  {
+    page: 4,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=4.jpg&t=RyknR6dCvGvBxdT&scalingup=0",
+  },
+  {
+    page: 5,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=5.jpg&t=lt0gQNCetQ0bueM&scalingup=0",
+  },
+  {
+    page: 6,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=6.jpg&t=M9ACEytkuSvVoPf&scalingup=0",
+  },
+  {
+    page: 7,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=7.jpg&t=IZp0PLwr56nJrIH&scalingup=0",
+  },
+  {
+    page: 8,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=8.jpg&t=KrHaUxeAFnBn43Z&scalingup=0",
+  },
+  {
+    page: 9,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=9.jpg&t=rZiHMXcYUZ6fakt&scalingup=0",
+  },
+  {
+    page: 10,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=10.jpg&t=Trm6kMumzsSFUs7&scalingup=0",
+  },
+  {
+    page: 11,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=11.jpg&t=naLdIpkXmMhtmHn&scalingup=0",
+  },
+  {
+    page: 12,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=12.jpg&t=iMWePvcfzeBXHja&scalingup=0",
+  },
+  {
+    page: 13,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=13.jpg&t=J1vzLohNDHgxiaP&scalingup=0",
+  },
+  {
+    page: 14,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=14.jpg&t=1sIzwNjISsf9aM2&scalingup=0",
+  },
+  {
+    page: 15,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=15.jpg&t=B2OeSqZi0Ft0GiM&scalingup=0",
+  },
+  {
+    page: 16,
+    src: "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=lolivo-16.jpg&t=gk67KAbBXXDEFzw&scalingup=0",
+  },
+];
 function MyMenu(props: any) {
   return (
     <HTMLFlipBook
@@ -48,134 +113,15 @@ function MyMenu(props: any) {
       // children={undefined}
       minWidth={0}
     >
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=1.jpg&t=JQPxvHjzMkqD6xJ&scalingup=0"
-        }
-        alt="menu-1"
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=2.jpg&t=HtaKxxcMb9Gs37p&scalingup=0"
-        }
-        alt="menu-2"
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=3.jpg&t=764f0LsLEaD2URL&scalingup=0"
-        }
-        alt="menu-3"
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=4.jpg&t=RyknR6dCvGvBxdT&scalingup=0"
-        }
-        alt="menu-4"
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=5.jpg&t=lt0gQNCetQ0bueM&scalingup=0"
-        }
-        alt="menu-5"
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=6.jpg&t=M9ACEytkuSvVoPf&scalingup=0"
-        }
-        alt="menu-6"
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=7.jpg&t=IZp0PLwr56nJrIH&scalingup=0"
-        }
-        alt="menu-7"
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=8.jpg&t=KrHaUxeAFnBn43Z&scalingup=0"
-        }
-        alt="menu-8"
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=9.jpg&t=rZiHMXcYUZ6fakt&scalingup=0"
-        }
-        alt="menu-9"
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=10.jpg&t=Trm6kMumzsSFUs7&scalingup=0"
-        }
-        alt="menu-10 "
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=11.jpg&t=naLdIpkXmMhtmHn&scalingup=0"
-        }
-        alt="menu-11 "
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=12.jpg&t=iMWePvcfzeBXHja&scalingup=0"
-        }
-        alt="menu-12 "
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=13.jpg&t=J1vzLohNDHgxiaP&scalingup=0"
-        }
-        alt="menu-13 "
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=14.jpg&t=1sIzwNjISsf9aM2&scalingup=0"
-        }
-        alt="menu-14 "
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=15.jpg&t=B2OeSqZi0Ft0GiM&scalingup=0"
-        }
-        alt="menu-15 "
-        width="300"
-        height="300"
-      />
-      <Image
-        src={
-          "https://lolivo.bg/images/admin/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=561&a=true&file=lolivo-16.jpg&t=gk67KAbBXXDEFzw&scalingup=0"
-        }
-        alt="last-page"
-        width="300"
-        height="300"
-      />
+      {menuPages.map((page: any, index: number) => (
+        <Image
+          key={index}
+          src={page.src}
+          alt={`MENU-PAGE-${index}`}
+          width="300"
+          height="300"
+        />
+      ))}
     </HTMLFlipBook>
   );
 }
