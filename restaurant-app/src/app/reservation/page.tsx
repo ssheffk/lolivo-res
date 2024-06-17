24a67631adc17049a2acf974ec9972ee4f22b9f2
Serdar Shefketov <serdar.shefketov@lights.digital>
2024-06-17 10:19:22 +0300

    (lolivo):kids center phone number

--- restaurant-app/src/app/reservation/page.tsx
+++ restaurant-app/src/app/reservation/page.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ArchWithStar from "../components/ArcWithStar";
-import { PageWrapper } from "../PageWrapper";
+import React from 'react';
+import ArchWithStar from '../components/ArcWithStar';
+import { PageWrapper } from '../PageWrapper';
 
 const ReservationPage: React.FC<any> = ({ header, footer, page }) => {
   return (
@@ -10,7 +10,7 @@ const ReservationPage: React.FC<any> = ({ header, footer, page }) => {
           <ArchWithStar />
           <div className="relative px-4 max-w-[400px] mx-auto lg:max-w-[808px] xl:px-0">
             <h1 className="text-xl leading-none font-Gloock uppercase text-center mb-10 lg:text-5xl lg:leading-none lg:mb-14">
-              Телефон за резервации
+              Телефон за ресторанта
             </h1>
             <div className="grid grid-cols-1 gap-4 max-w-[850px] mx-auto lg:gap-6">
               <div className="border border-[#DBD9D5] rounded-[7px] p-6">
@@ -20,6 +20,23 @@ const ReservationPage: React.FC<any> = ({ header, footer, page }) => {
                 <div className="content text-sm leading-normal tracking-[-0.41px] text-appGray-200 lg:text-lg lg:leading-normal">
                   <div className="content-primitive content--paragraph">
                     <p>0882 244 244</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative px-4 max-w-[400px] mx-auto lg:max-w-[808px] xl:px-0">
+            <h1 className="text-xl leading-none font-Gloock uppercase text-center mb-10 lg:text-5xl lg:leading-none lg:mb-14">
+              Телефон за детски кът
+            </h1>
+            <div className="grid grid-cols-1 gap-4 max-w-[850px] mx-auto lg:gap-6">
+              <div className="border border-[#DBD9D5] rounded-[7px] p-6">
+                <h2 className="leading-none tracking-[-0.41px] font-Gloock mb-[14px] lg:text-[32px] lg:leading-none lg:mb-5">
+                  Мениджър
+                </h2>
+                <div className="content text-sm leading-normal tracking-[-0.41px] text-appGray-200 lg:text-lg lg:leading-normal">
+                  <div className="content-primitive content--paragraph">
+                    <p>0886 288 288</p>
                   </div>
                 </div>
               </div>
