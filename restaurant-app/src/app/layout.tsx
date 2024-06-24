9121a3592bbeec223140a80aa5b6b4149c6881e7
Serdar Shefketov <serdar.shefketov@lights.digital>
2024-06-24 15:49:31 +0300

    (lolivo):kids center phone number

--- restaurant-app/src/app/layout.tsx
+++ restaurant-app/src/app/layout.tsx
@@ -1,15 +1,18 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-import "./../../assets/css/fonts.css";
-import "./../../assets/css/main.css";
-import "./../../assets/css/transition.css";
+import './../../assets/css/fonts.css';
+import './../../assets/css/main.css';
+import './../../assets/css/transition.css';
 
-const inter = Inter({ subsets: ["latin"] });
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "L'Olivo",
-  description: "Ресторант с детски кът Neverland",
+  description: 'Ресторант с детски кът Neverland',
 };
 
 export default function RootLayout({
@@ -19,7 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   );
 }
