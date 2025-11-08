563398e97d8e5ff9e99d6f82e0b8ba3a112579f9
serjs <serdar.shefketov@lights.digital>
2025-11-08 18:48:30 +0200

    new images and price

--- restaurant-app/src/app/components/MyMenu.tsx
+++ restaurant-app/src/app/components/MyMenu.tsx
@@ -1,88 +1,88 @@
-'use client';
-import React from 'react';
-import HTMLFlipBook from 'react-pageflip';
-import Page_1 from '1.jpg';
-import Page_2 from '2.jpg';
-import Page_3 from '3.jpg';
-import Page_4 from '4.jpg';
-import Page_5 from '5.jpg';
-import Page_6 from '6.jpg';
-import Page_7 from '7.jpg';
-import Page_8 from '8.jpg';
-import Page_9 from '9.jpg';
-import Page_10 from '10.jpg';
-import Page_11 from '11.jpg';
-import Page_12 from '12.jpg';
-import Page_13 from '13.jpg';
-import Page_14 from '14.jpg';
-import Page_15 from '15.jpg';
-import Page_16 from '16.jpg';
+"use client";
+import React from "react";
+import HTMLFlipBook from "react-pageflip";
 
-import Image from 'next/image';
+import Image from "next/image";
 const menuPages = [
   {
     page: 1,
-    src: '/1.jpg',
+    src: "/1.jpg",
   },
   {
     page: 2,
-    src: '/2.jpg',
+    src: "/2.jpg",
   },
   {
     page: 3,
-    src: '/3.jpg',
+    src: "/3.jpg",
   },
   {
     page: 4,
-    src: '/4.jpg',
+    src: "/4.jpg",
   },
   {
     page: 5,
-    src: '/5.jpg',
+    src: "/5.jpg",
   },
   {
     page: 6,
-    src: '/6.jpg',
+    src: "/6.jpg",
   },
   {
     page: 7,
-    src: '/7.jpg',
+    src: "/7.jpg",
   },
   {
     page: 8,
-    src: '/8.jpg',
+    src: "/8.jpg",
   },
   {
     page: 9,
-    src: '/9.jpg',
+    src: "/9.jpg",
   },
   {
     page: 10,
-    src: '/10.jpg',
+    src: "/10.jpg",
   },
   {
     page: 11,
-    src: '/11.jpg',
+    src: "/11.jpg",
   },
   {
     page: 12,
-    src: '/12.jpg',
+    src: "/12.jpg",
   },
   {
     page: 13,
-    src: '/13.jpg',
+    src: "/13.jpg",
   },
   {
     page: 14,
-    src: '/14.jpg',
+    src: "/14.jpg",
   },
   {
     page: 15,
-    src: '/15.jpg',
+    src: "/15.jpg",
   },
   {
     page: 16,
-    src: '/16.jpg',
+    src: "/16.jpg",
+  },
+  {
+    page: 17,
+    src: "/17.jpg",
+  },
+  {
+    page: 18,
+    src: "/18.jpg",
+  },
+  {
+    page: 19,
+    src: "/19.jpg",
+  },
+  {
+    page: 20,
+    src: "/20.jpg",
   },
 ];
 function MyMenu(props: any) {
