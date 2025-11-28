4d2b3689187040cc964b12a92b06e73c27180870
serjs <serdar.shefketov@lights.digital>
2025-11-28 20:11:11 +0200

    new images and price

--- restaurant-app/src/app/menu/page.tsx
+++ restaurant-app/src/app/menu/page.tsx
@@ -1,10 +1,10 @@
-import { PageWrapper } from '../PageWrapper';
-import ArchWithStar from '../components/ArcWithStar';
-import MyMenu from '../components/MyMenu';
+import { PageWrapper } from "../PageWrapper";
+import ArchWithStar from "../components/ArcWithStar";
+import MyMenu from "../components/MyMenu";
 
 const MenuPage: React.FC = () => {
   return (
-    <PageWrapper page={'page'} header={'header'} footer={'footer'}>
+    <PageWrapper page={"page"} header={"header"} footer={"footer"}>
       <section className="pt-[108px] pb-10 overflow-hidden md:pb-20 lg:pt-[218px] lg:pb-[120px] ">
         <h1 className="text-xl leading-none font-Gloock uppercase text-center mb-6 lg:text-5xl lg:leading-none">
           Меню
@@ -19,7 +19,7 @@ const MenuPage: React.FC = () => {
             <div className="lg:py-12 p-4">
               <img
                 alt=""
-                src="./menu-new.jpg"
+                src="./menu-new.png"
                 className=" w-full  object-contain h-[600px]"
               />
             </div>
